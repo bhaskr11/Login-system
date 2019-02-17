28a3b46d5ad737903c920a258bfee76cec9fbbb1
bhaskr11 <bhaskar_nhuchhe@hotmail.com>
2019-02-16 23:13:01 -0500

    added cofig, footer, login and register pages

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php 
+    //IF NO CONFIG CONSTANT THEN DO NOT LOAD FILE
+    if(!defined('__CONFIG__')){
+        exit("You do not have a config file! :(");
+    }
+
+    
+
+?> 
---
+++ register.php
@@ -0,0 +1,47 @@
+<?php 
+    //Allowing the config 
+    define('__CONFIG__', true);
+    //Require the config file
+    require_once "inc/config.php"; 
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <title>Page Title!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <form class="js-register">
+        <h2>Register</h2>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required = "required">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required = 'required'>
+        </div>
+
+        <!-- <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div> -->
+        
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+
+    <?php require_once "inc/footer.php" ?>
+
+  </body>
+</html>
